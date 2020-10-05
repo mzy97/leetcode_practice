9bf260fd958af52987788151264517056e020447
穆正阳 <muzhengyang@muzhengyangdeMacBook-Pro.local>
2020-10-05 13:49:17 +0800

    init commit

---
+++ twosum.py
@@ -0,0 +1,20 @@
+"""
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+https://leetcode-cn.com/problems/two-sum
+
+"""
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
