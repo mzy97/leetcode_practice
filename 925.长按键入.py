164f301ed4727fa0fe879bdc0d99d77348e59cfb
穆正阳 <mzy97@bupt.edu.cn>
2020-10-21 23:27:32 +0800

    commit

---
+++ 925.长按键入.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=925 lang=python3
+#
+# [925] 长按键入
+#
+
+# @lc code=start
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i,j = 0,0
+
+        while j<len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif j > 0 and typed[j] == typed[j-1]:
+                j += 1
+            else:
+                return False
+        
+        return i == len(name)
+# @lc code=end
+
